32f1758541146063245e382b96b2069cbe4410a8
ssomnath <somnaths@ornl.gov>
2018-06-22 13:04:29 -0400

    Simplifying example generation

--- docs/conf.py
+++ docs/conf.py
@@ -406,11 +406,10 @@ intersphinx_mapping = {'python': ('https://docs.python.org/{.major}'.format(sys.
 # -- Options for todo extension ----------------------------------------------
 
 # -------------------------------------------------
-from sphinx_gallery.sorting import ExampleTitleSortKey, ExplicitOrder #Can't use until next release of sphinx-gallery
+from sphinx_gallery.sorting import ExampleTitleSortKey, ExplicitOrder  # Can't use until next release of sphinx-gallery
 # Sphinx-gallery configuration
-sphinx_gallery_conf = dict(examples_dirs=['../examples',
-                                          '../examples/cookbooks'],
-                           gallery_dirs=['auto_examples', 'auto_tutorials'],
+sphinx_gallery_conf = dict(examples_dirs=['../examples'],
+                           gallery_dirs=['auto_examples'],
                            within_subsection_order=ExampleTitleSortKey,
                            reference_url=dict(pyUSID=None,
                                               matplotlib='https://matplotlib.org',
