f34495c2ad8b9815666df536af0be6776727fe08
ssomnath <somnaths@ornl.gov>
2018-08-02 17:07:22 -0400

    Fixed broken links to python file

--- examples/intermediate/plot_process.py
+++ examples/intermediate/plot_process.py
@@ -98,7 +98,7 @@
 #
 #        a. Save the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py>`_ as ``peak_finding.py`` in the
 #           same folder as the notebook from step 1.
-#        b. Copy the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py`_ into the jupyter notebook
+#        b. Copy the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py>`_ into the jupyter notebook
 #           downloaded in step 1. The contents of the file need to replace the last three lines in the cell below:
 #           ``import sys`` to ``from peak_finding import find_all_peaks``
 #
