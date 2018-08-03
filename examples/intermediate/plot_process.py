1cca75bfc174ea2dac9dfac881ae6908499515e2
ssomnath <somnaths@ornl.gov>
2018-08-03 12:30:31 -0400

    forcing people to only download the file instead of adding to notebook

--- examples/intermediate/plot_process.py
+++ examples/intermediate/plot_process.py
@@ -94,13 +94,8 @@
 #     In order to run this document on your own computer, you will need to:
 #
 #     1. Download the document as a Jupyter notebook using the link at the bottom of this page.
-#     2. Get the ``find_all_peaks()`` function via either of the approaches:
-#
-#        a. Save the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py>`_ as ``peak_finding.py`` in the
-#           same folder as the notebook from step 1.
-#        b. Copy the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py>`_ into the jupyter notebook
-#           downloaded in step 1. The contents of the file need to replace the last three lines in the cell below:
-#           ``import sys`` to ``from peak_finding import find_all_peaks``
+#     2. Save the contents of `this python file <https://github.com/pycroscopy/pyUSID/blob/master/examples/intermediate/supporting_docs/peak_finding.py>`_ as ``peak_finding.py`` in the
+#        same folder as the notebook from step 1.
 #
 # Import necessary packages
 # -------------------------
