3ac25e40c3fe8b873af5ce995dd2af9a476c467f
ssomnath <somnaths@ornl.gov>
2018-10-15 17:09:09 -0400

    Thoroughly revised and completed example on 1D datasets

--- examples/usid_representation/plot_1D_spectrum.py
+++ examples/usid_representation/plot_1D_spectrum.py
@@ -7,10 +7,26 @@
 
 10/12/2018
 
-**This document is under construction.**
+**This example illustrates how a 1 dimensional curve or spectrum would be represented in the Universal Spectroscopy and
+Imaging Data (USID) schema and stored in a Hierarchical Data Format (HDF5) file, also referred to as the h5USID file.**
 
-Please consider downloading this document as a Jupyter notebook using the
-button at the bottom of this document.
+This document is intended as a supplement to the explanation about the `USID data model </../../data_format.html>`_
+
+Please consider downloading this document as a Jupyter notebook using the button at the bottom of this document.
+
+Prerequisites:
+--------------
+We recommend that you read about the `USID data model </../../data_format.html>`_
+
+We will be making use of the ``pyUSID`` package at multiple places to illustrate the central point. While it is
+recommended / a bonus, it is not absolutely necessary that the reader understands how the specific ``pyUSID`` functions
+work or why they were used in order to understand the data representation itself.
+Examples about these functions can be found in other documentation on pyUSID and the reader is encouraged to read the
+supplementary documents.
+
+Import all necessary packages
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The main packages necessary for this example are ``h5py`` and ``matplotlib`` in addition to ``pyUSID``:
 """
 from __future__ import print_function, division, unicode_literals
 import subprocess
@@ -47,6 +63,9 @@ except ImportError:
 ########################################################################################################################
 # Download the dataset
 # ---------------------
+# We will be working on a **Force-Distance Curve** obtained from an Atomic Force Microscope (AFM) in this example.
+# As mentioned earlier, this dataset is available on the pyUSID repository and can be accessed directly as well.
+# Here, we will simply download the file using ``wget``:
 h5_path = 'temp.h5'
 url = 'https://raw.githubusercontent.com/pycroscopy/pyUSID/master/data/AFM_Force_Curve.h5'
 if os.path.exists(h5_path):
@@ -56,85 +75,118 @@ _ = wget.download(url, h5_path, bar=None)
 ########################################################################################################################
 # Open the file
 # -------------
-# Look at the contents of the file
+# Lets open the file and look at its contents using
+# `pyUSID.hdf_utils.print_tree() <../beginner/plot_hdf_utils_read.html#print-tree>`_
 h5_file = h5py.File(h5_path, mode='r')
 usid.hdf_utils.print_tree(h5_file)
 
 ########################################################################################################################
-# Notice that this file has multiple Channels, each with a dataset named ``Raw_Data``. Are they all
-# ``Main`` datasets? There are multiple ways to find out this. One approach is simply to ask pyUSID to list out all
-# available ``Main`` datasets.
-
-for main_dset in usid.hdf_utils.get_all_main(h5_file):
-    print(main_dset)
-
-########################################################################################################################
-# Turns out that each of those datasets named ``Raw_Data`` were indeed ``Main`` datasets. But, how could they be if the
-# are not co-located with the four mandatory ancillary datasets within each channel?
-# Recall from the USID documentation that:
-#
-# 1. Multiple ``Main`` datasets can share the same ``Ancillary`` datasets
-# 2. The ``Main`` datasets only need to have ``attributes`` named ``Position_Indices`` with the value set to the
-#    reference of appropriate ``Ancillary`` datasets
-# We can investigate if this is indeed the case here.2
-
-########################################################################################################################
-# Visualize the contents in each of these channels
-# ------------------------------------------------
-usid.plot_utils.use_nice_plot_params()
-for main_dset in usid.hdf_utils.get_all_main(h5_file):
-    main_dset.visualize()
-
-########################################################################################################################
-# Access the ``Main`` Dataset containing the spectrum of interest
-# ---------------------------------------------------------------
-# In this case, this is the second channel:
-h5_main = usid.hdf_utils.get_all_main(h5_file)[1]
+# Clearly, this file contains a single `Measurement` which has a single `Channel </../../data_format.html#channels>`_.
+# We can access the `Main Dataset </../../data_format.html#main-datasets>`_ where all the information is located in
+# multiple ways. Given that this file contains just a single ``Main Dataset`` we can conveniently use the
+# `pyUSID.hdf_utils.get_all_main() <../beginner/plot_hdf_utils_read.html#get-all-main>`_ function.
+h5_main = usid.hdf_utils.get_all_main(h5_file)[-1]
 print(h5_main)
 
 ########################################################################################################################
-# Look at the Position Indices dataset linked to the Main dataset
-# ---------------------------------------------------------------
+# Here, ``h5_main`` is a `USIDataset <../beginner/plot_usi_dataset.html>`_, which can be thought of as a supercharged
+# HDF5 Dataset that is not only aware of the contents of the plain ``Raw_Data`` dataset but also its links to the
+# `Ancillary Datasets </../../data_format.html#ancillary-datasets>`_ that make it a ``Main Dataset``.
+#
+# Understanding Dimensionality
+# ----------------------------
+# What is more is that the above print statement shows that this ``Main Dataset`` has one ``Position Dimension`` - ``X``
+# which was varied over a single value and a single ``Spectroscopic Dimension`` - ``Z`` which was varied several times.
+# Therefore, this dataset is really just a simple 1D dataset where the sole dimension is ``Z``.
+#
+# The original shape of this dataset would be ``(2045,)``. In USID, this shape needs to (explicitly) include a position
+# axis to state that the measurement was acquired over a single position / location. Therefore, the shape of this data
+# in USID would be ``(1, 2045)``
+#
+# Visualize the Main Dataset
+# --------------------------
+# Now lets visualize the contents within this ``Main Dataset`` using the ``USIDataset's`` built-in
+# `visualize() <../beginner/plot_usi_dataset.html#interactive-visualization>`_ function. Clearly, this dataset is indeed
+# a simple 1D dataset.
+usid.plot_utils.use_nice_plot_params()
+h5_main.visualize()
+
+########################################################################################################################
+# Ancillary Datasets
+# ------------------
+# As mentioned in the documentation on USID, ``Ancillary Datasets`` are required to complete the information for any
+# dataset. Specifically, these datasets need to provide information about the values against which measurements were
+# acquired, in addition to explaining the original dimensionality (1 in this case) of the original dataset. Let's look
+# at the ancillary datasets and see what sort of information they provide. We can access the ``Ancillary Datasets``
+# linked to the ``Main Dataset`` (``h5_main``) just like a property of the object.
+#
+# Ancillary Position Datasets
+# ---------------------------
+# Given that this force-distance curve was acquired at a single position, the ancillary position datasets become trivial
+# as seen below. Note that we access the ``Ancillary Datasets`` as properties of ``h5_main``:
+print('Position Indices:')
+print('-------------------------')
 print(h5_main.h5_pos_inds)
-
-########################################################################################################################
-# Look at the Position Values dataset linked to the Main dataset
-# ---------------------------------------------------------------
+print('containing:')
+print(h5_main.h5_pos_inds[()])
+print('\nPosition Values:')
+print('-------------------------')
 print(h5_main.h5_pos_vals)
+print('containing:')
+print(h5_main.h5_pos_vals[()])
 
 ########################################################################################################################
-# See the attributes within the Position Indices Dataset
-# ------------------------------------------------------
+# Note that as explained above, these position datasets only contain a single value for the `fake` position axis over
+# which the data was collected. Regardless of the fact that the data was collected at a single location, the Position
+# datasets should be two dimensional in shape (one element in each axis) according the USID rules.
+#
+# Regardless of how uninformative the Position Datasets seem for this specific example, they are still necessary for the
+# ``Raw_Data`` dataset to be a ``Main Dataset``.
+#
+# Attributes associated with the Position Indices Dataset
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Just looking at the shape and values of the Position ancillary datasets does not provide all the information.
+# Recall that the ancillary datasets need to have some mandatory attributes like ``labels`` and ``units`` that
+# describe the quantity and units for each of the dimensions:
 for key, val in usid.hdf_utils.get_attributes(h5_main.h5_pos_inds).items():
     print('{} : {}'.format(key, val))
 
 ########################################################################################################################
-# See the contents within the Position Datasets
-# --------------------------------------------------
-print(h5_main.h5_pos_inds[()], h5_main.h5_pos_vals[()])
-
-########################################################################################################################
-# Look at the Spectroscopic Indices dataset linked to the Main dataset
-# --------------------------------------------------------------------
-# Given that this is a 2D image where data was not acquired as a function of some independent parameter at each
-# location, the spectroscopic datasets will contain bare minimum information
+# Ancillary Spectroscopic Datasets
+# --------------------------------
+# Unlike the Position ``Ancillary Datasets`` that are not very descriptive for this example, the Spectroscopic Datasets
+# are very important since these datasets explain how the independent variable, ``Z`` was varied.
+#
+# Looking at the Spectroscopic HDF5 datasets
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print('Spectroscopic Indices:')
+print('-------------------------')
 print(h5_main.h5_spec_inds)
-
-########################################################################################################################
-# Look at the Spectroscopic Values dataset linked to the Main dataset
-# --------------------------------------------------------------------
+print('\nSpectroscopic Values:')
+print('-------------------------')
 print(h5_main.h5_spec_vals)
 
 
 ########################################################################################################################
+# Here, the first index in the shape of both datasets indicates that there is a single Spectroscopic Dimension.
+# The second index in the shape of the datasets indicates that this single dimension was varied over several values.
+#
 # See the attributes within the Spectroscopic Indices Dataset
-# ------------------------------------------------------------
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Again, the attributes of Spectroscopic Datasets show mandatory information about the Spectroscopic dimensions such as
+# the quantity (``labels``) and ``units``:
 for key, val in usid.hdf_utils.get_attributes(h5_main.h5_spec_inds).items():
     print('{} : {}'.format(key, val))
 
 ########################################################################################################################
-# Visualize the contents of the Position Indices Dataset
-# ------------------------------------------------------
+# Visualize the contents of the Spectroscopic Datasets
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Observe the single curve that is associated with the single spectroscopic variable ``Z``. Also note that the contents
+# of the ``Spectroscopic Indices`` dataset are just a linearly increasing set of numbers starting from ``0`` according
+# to the definition of the Indices datasets which just count the nth value of independent variable that was varied.
+# The ``Spectroscopic Values`` dataset clearly shows that this variable varies non-linearly and could not have been
+# represented using trivial bookkeeping. **This ability to allow dimensions to vary in arbitrary manners is one of the
+# biggest strengths of USID over other alternatives.**
 fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
 for axis, data, title, y_lab in zip(axes.flat,
                                     [h5_main.h5_spec_inds[()].T, h5_main.h5_spec_vals[()].T],
@@ -152,8 +204,11 @@ fig.tight_layout()
 ########################################################################################################################
 # Clean up
 # --------
-# Finally lets close and delete the example HDF5 file
-
+# Finally lets close the HDF5 file. and delete the example HDF5 file
 h5_file.close()
+
+########################################################################################################################
+# Here, we will even delete the HDF5 file. Please comment out this line if you want to look at the HDF5 file using
+# software like HDFView.
 os.remove(h5_path)
 
