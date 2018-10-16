b0f66e418b6f079b3906f4ac0dd04d04becf4719
ssomnath <somnaths@ornl.gov>
2018-10-16 13:04:03 -0400

    First completed draft of the 2D image stack example

--- examples/usid_representation/plot_2D_image_stack.py
+++ examples/usid_representation/plot_2D_image_stack.py
@@ -7,10 +7,29 @@
 
 10/12/2018
 
-**This document is under construction.**
+**This example illustrates how a set of 2D grayscale images would be represented in the Universal Spectroscopy and
+Imaging Data (USID) schema and stored in a Hierarchical Data Format (HDF5) file, also referred to as the h5USID file.**
+This example is based on the popular Atomic Force Microscopy scan mode where multiple sensors *simultaneously* acquire
+a value at each position on a 2D grid, thereby resulting in a 2D image per sensor. Specifically, the goal of this
+example is to demonstrate the sharing of ``Ancillary`` datasets among multiple ``Main`` datasets.
 
-Please consider downloading this document as a Jupyter notebook using the
-button at the bottom of this document.
+This document is intended as a supplement to the explanation about the `USID data model </.../data_format.html>`_
+
+Please consider downloading this document as a Jupyter notebook using the button at the bottom of this document.
+
+Prerequisites:
+--------------
+We recommend that you read about the `USID data model </.../data_format.html>`_
+
+We will be making use of the ``pyUSID`` package at multiple places to illustrate the central point. While it is
+recommended / a bonus, it is not absolutely necessary that the reader understands how the specific ``pyUSID`` functions
+work or why they were used in order to understand the data representation itself.
+Examples about these functions can be found in other documentation on pyUSID and the reader is encouraged to read the
+supplementary documents.
+
+Import all necessary packages
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The main packages necessary for this example are ``h5py`` and ``matplotlib`` in addition to ``pyUSID``:
 """
 from __future__ import print_function, division, unicode_literals
 import subprocess
@@ -47,6 +66,8 @@ except ImportError:
 ########################################################################################################################
 # Download the dataset
 # ---------------------
+# As mentioned earlier, this image is available on the pyUSID repository and can be accessed directly as well.
+# Here, we will simply download the file using ``wget``:
 h5_path = 'temp.h5'
 url = 'https://raw.githubusercontent.com/pycroscopy/pyUSID/master/data/SingFreqPFM_0003.h5'
 if os.path.exists(h5_path):
@@ -56,123 +77,74 @@ _ = wget.download(url, h5_path, bar=None)
 ########################################################################################################################
 # Open the file
 # -------------
-# Look at the contents of the file
+# Lets open the file and look at its contents using
+# `pyUSID.hdf_utils.print_tree() <../beginner/plot_hdf_utils_read.html#print-tree>`_
+
 h5_file = h5py.File(h5_path, mode='r')
 usid.hdf_utils.print_tree(h5_file)
 
 ########################################################################################################################
-# Notice that there are multiple channels with a single dataset named ``Raw_Data`` in them. From the `1D Spectrum`
-# example, we now know that these ``Raw_Data`` datasets are actually ``Main`` datasets and they share the same
-# set of ancillary datasets that are under ``Measurement_000`` group. This is common for Scanning Probe Microscopy
-# scans where information from multiple sensors are recorded **simultaneously** during the scan.
+# Notice that this file has multiple `Channels </../../data_format.html#channels>`_, each with a dataset named
+# ``Raw_Data``. Are they all `Main </../../data_format.html#main-datasets>`_ datasets?
+# There are multiple ways to find out this. One approach is simply to ask pyUSID to list out all available ``Main``
+# datasets.
 #
 # Visualize the contents in each of these channels
 # ------------------------------------------------
+
+for main_dset in usid.hdf_utils.get_all_main(h5_file):
+    print(main_dset)
+    print('---------------------------------------------------------------\n')
+
+########################################################################################################################
+# From the print statements above, it is clear that each of these ``Raw_Data`` datasets were indeed ``Main`` datasets.
+# How can these datasets be ``Main`` if they are not co-located with the corresponding sets of ``Ancillary`` datasets
+# within each ``Channel`` group?
+#
+# Sharing Ancillary Datasets
+# --------------------------
+# Since each of the ``Main`` datasets have the same position and spectroscopic dimensions, they share the same
+# set of ancillary datasets that are under ``Measurement_000`` group. This is common for Scanning Probe Microscopy
+# scans where information from multiple sensors are recorded **simultaneously** during the scan.
+#
+# Recall from the USID documentation that:
+#
+# 1. Multiple ``Main`` datasets can share the same ``Ancillary`` datasets
+# 2. The ``Main`` datasets only need to have ``attributes`` named ``Position_Indices``, ``Position_Values``,
+#    ``Spectroscopic_Indices``, and ``Spectroscopic_Values``with the value set to the reference of the corresponding
+#    ``Ancillary`` datasets
+#
+# We can investigate if this is indeed the case here. Lets get the references to ``Ancillary`` datasets linked to each
+# of the ``Main`` datasets:
+
+for main_dset in usid.hdf_utils.get_all_main(h5_file):
+    print('Main Dataset: {}'.format(main_dset.name))
+    print('Position Indices: {}'.format(main_dset.h5_pos_inds.name))
+    print('Position Values: {}'.format(main_dset.h5_pos_vals.name))
+    print('Spectroscopic Indices: {}'.format(main_dset.h5_spec_inds.name))
+    print('Spectroscopic Values: {}'.format(main_dset.h5_spec_vals.name))
+    print('---------------------------------------------------------------\n')
+
+########################################################################################################################
+# From above, we see that all the ``Main`` datasets we indeed referencing the same set of ``Ancillary`` Datasets.
+# Note that it would **not** have been wrong to store (duplicate copies of) the ``Ancillary`` datasets within each
+# ``Channel`` group. The data was stored in this manner since it is more efficient and because it was known *apriori*
+# that all ``Main`` datasets are dimensionally equal. Also note that this sharing of ``Ancillary`` datasets is OK even
+# though the physical quantity and units within each ``Main`` dataset are different since these two pieces of
+# information are stored in the attributes of the ``Main`` datasets (which are unique and independent) and not in the
+# ``Ancillary`` datasets.
+#
+# The discussion regarding the contents of the ``Ancillary`` datasets are identical to that for the `2D grayscale
+# image <./plot_2D_image.html>`_ and will not be discussed here for brevity.
+#
+# Visualizing the contents within each channel
+# --------------------------------------------
+# Now lets visualize the contents within this ``Main Dataset`` using the ``USIDataset's`` handy built-in
+# `visualize() <../beginner/plot_usi_dataset.html#interactive-visualization>`_ function.
+
 usid.plot_utils.use_nice_plot_params()
 for main_dset in usid.hdf_utils.get_all_main(h5_file):
-    main_dset.visualize()
-
-########################################################################################################################
-# Access the ``Main`` Dataset containing the image of interest
-# ------------------------------------------------------------
-h5_main = usid.hdf_utils.get_all_main(h5_file)[-1]
-print(h5_main)
-
-########################################################################################################################
-# Look at the Position Indices dataset linked to the Main dataset
-# ---------------------------------------------------------------
-print(h5_main.h5_pos_inds)
-
-########################################################################################################################
-# Look at the Position Values dataset linked to the Main dataset
-# ---------------------------------------------------------------
-print(h5_main.h5_pos_vals)
-
-########################################################################################################################
-# See the attributes within the Position Indices Dataset
-# ------------------------------------------------------
-for key, val in usid.hdf_utils.get_attributes(h5_main.h5_pos_inds).items():
-    print('{} : {}'.format(key, val))
-
-
-########################################################################################################################
-# Visualize the contents of the Position Indices Dataset
-# ------------------------------------------------------
-fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
-axes[0].plot(h5_main.h5_pos_inds[()])
-axes[0].set_title('Full dataset')
-axes[1].set_title('First 1024 rows only')
-axes[1].plot(h5_main.h5_pos_inds[:1024])
-for axis in axes.flat:
-    axis.set_xlabel('Row in Position Indices')
-    axis.set_ylabel('Position Indices')
-    axis.legend(['X', 'Y'])
-fig.suptitle('Position Indices dataset')
-fig.tight_layout()
-
-########################################################################################################################
-# Visualize the contents of the Position Values Dataset
-# ------------------------------------------------------
-fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
-axes[0].plot(h5_main.h5_pos_vals[()])
-axes[0].set_title('Full dataset')
-axes[1].set_title('First 1024 rows only')
-axes[1].plot(h5_main.h5_pos_vals[:1024])
-for axis in axes.flat:
-    axis.set_xlabel('Row in Position Values')
-    axis.set_ylabel('Position Values')
-    axis.legend(['X', 'Y'])
-fig.suptitle('Position Values dataset')
-fig.tight_layout()
-
-########################################################################################################################
-# Look at the Spectroscopic Indices dataset linked to the Main dataset
-# --------------------------------------------------------------------
-# Given that this is a 2D image where data was not acquired as a function of some independent parameter at each
-# location, the spectroscopic datasets will contain bare minimum information
-print(h5_main.h5_spec_inds)
-
-########################################################################################################################
-# Look at the Spectroscopic Values dataset linked to the Main dataset
-# --------------------------------------------------------------------
-print(h5_main.h5_spec_vals)
-
-
-########################################################################################################################
-# See the attributes within the Spectroscopic Indices Dataset
-# ------------------------------------------------------------
-for key, val in usid.hdf_utils.get_attributes(h5_main.h5_spec_inds).items():
-    print('{} : {}'.format(key, val))
-
-########################################################################################################################
-# See the contents within the Spectroscopic Datasets
-# --------------------------------------------------
-print(h5_main.h5_spec_inds[()], h5_main.h5_spec_vals[()])
-
-
-########################################################################################################################
-# Attempting to visualize the first few rows of the image manually
-# ----------------------------------------------------------------
-print(h5_main.pos_dim_labels)
-print(h5_main.pos_dim_sizes)
-
-
-########################################################################################################################
-rows_to_read = 50
-num_cols = h5_main.pos_dim_sizes[1]
-first_few_rows_1D = h5_main[: rows_to_read * num_cols, :]
-print(first_few_rows_1D.shape)
-
-
-########################################################################################################################
-first_few_rows_2D = np.reshape(first_few_rows_1D, (rows_to_read, num_cols))
-print(first_few_rows_2D.shape)
-
-
-########################################################################################################################
-fig, axis = plt.subplots()
-axis.imshow(first_few_rows_2D, origin='lower')
-
+    main_dset.visualize(num_ticks=3, stdevs=2)
 
 ########################################################################################################################
 # Clean up
