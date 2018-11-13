6e736dcc3125818d8838bd75f47b33139c6d6788
ssomnath <somnaths@ornl.gov>
2018-11-13 14:22:30 -0500

    Moved computational tests from io_utils to comp_utils

--- tests/io/test_io_utils.py
+++ tests/io/test_io_utils.py
@@ -7,59 +7,12 @@ Created on Tue Nov  3 15:07:16 2017
 from __future__ import division, print_function, unicode_literals, absolute_import
 import unittest
 import sys
-from multiprocessing import cpu_count
+
 sys.path.append("../../pyUSID/")
 from pyUSID.io import io_utils
 
-MAX_CPU_CORES = cpu_count()
-
 
 class TestIOUtils(unittest.TestCase):
-
-    def test_recommend_cores_many_small_jobs(self):
-        num_jobs = 14035
-        if MAX_CPU_CORES > 4:
-            min_free_cores = 2
-        else:
-            min_free_cores = 1
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
-        self.assertEqual(ret_val, max(1, MAX_CPU_CORES-min_free_cores))
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=1, lengthy_computation=False)
-        self.assertEqual(ret_val, 1)
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
-        self.assertEqual(ret_val, MAX_CPU_CORES)
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=5000, lengthy_computation=False)
-        self.assertEqual(ret_val, MAX_CPU_CORES)
-
-    def test_recommend_cores_changing_min_cores(self):
-        num_jobs = 14035
-        for min_free_cores in range(1, MAX_CPU_CORES):
-            ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
-            self.assertEqual(ret_val, max(1, MAX_CPU_CORES - min_free_cores))
-
-    def test_recommend_cores_illegal_min_free_cores(self):
-        num_jobs = 14035
-        min_free_cores = MAX_CPU_CORES
-        with self.assertRaises(ValueError):
-            _ = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
-
-    def test_reccomend_cores_few_small_jobs(self):
-        num_jobs = 13
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
-        self.assertEqual(ret_val, 1)
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
-        self.assertEqual(ret_val, 1)
-
-    def test_recommed_cores_few_large_jobs(self):
-        num_jobs = 13
-        if MAX_CPU_CORES > 4:
-            min_free_cores = 2
-        else:
-            min_free_cores = 1
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, lengthy_computation=True)
-        self.assertEqual(ret_val, max(1, MAX_CPU_CORES-min_free_cores))
-        ret_val = io_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES - 1, lengthy_computation=True)
-        self.assertEqual(ret_val, max(1, MAX_CPU_CORES - 1))
 
     def test_formatted_str_to_number(self):
         self.assertEqual(io_utils.formatted_str_to_number("4.32 MHz", ["MHz", "kHz"], [1E+6, 1E+3]), 4.32E+6)
