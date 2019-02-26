88de78518549b1598dd943330ef1dd7e0eec070d
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-02-26 15:06:43 -0500

    Fixed error in requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'pyUSID/__version__.py')) as f:
 # TODO: Move requirements to requirements.txt
 requirements = ['numpy>=1.13.0',
                 'h5py>=2.6.0',
-                'pillow'
+                'pillow',
                 'matplotlib>=2.0.0',
 
                 'psutil',
