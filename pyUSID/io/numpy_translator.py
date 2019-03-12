454c22fda17dd285a8b3ceb9fbba22611cc70a75
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-03-12 17:13:14 -0400

    UNTESTED generalization of NumpyTranslator that accepts Dask arrays NumpyArray is now just the ArrayTranslator

--- pyUSID/io/numpy_translator.py
+++ pyUSID/io/numpy_translator.py
@@ -12,8 +12,8 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 from os import path, remove
 import sys
 import h5py
-import numpy as np  # For array operations
-
+import numpy as np
+import dask.array as da
 from .write_utils import Dimension
 from .translator import Translator, generate_dummy_main_parms
 from .hdf_utils import write_main_dataset, write_simple_attrs
@@ -22,7 +22,7 @@ if sys.version_info.major == 3:
     unicode = str
 
 
-class NumpyTranslator(Translator):
+class ArrayTranslator(Translator):
     """
     Translator that writes numeric arrays (already in memory) that describe a USID dataset to a HDF5 file
     """
@@ -38,7 +38,7 @@ class NumpyTranslator(Translator):
             Absolute path of the h5 file to be written
         data_name : String / Unicode
             Name of the scientific data type. Example - 'SEM'
-        raw_data : np.ndarray
+        raw_data : :class:``np.ndarray` or :class:`dask.array.core.Array`
             2D matrix formatted as [position, spectral]
         quantity : String / Unicode
             Name of the physical quantity stored in the dataset. Example - 'Current'
@@ -52,11 +52,11 @@ class NumpyTranslator(Translator):
             Sequence of Dimension objects that provides all necessary instructions for constructing the
             indices and values datasets
             Object specifying the instructions necessary for building the Spectroscopic indices and values datasets
-        translator_name : String / unicode, Optional
+        translator_name : str / unicode, Optional
             Name of the translator. Example - 'HitachiSEMTranslator'
-        parm_dict : dictionary (Optional)
+        parm_dict : dict, Optional
             Dictionary of parameters that will be written under the group 'Measurement_000'
-        extra_dsets : dictionary (Optional)
+        extra_dsets : dict, Optional
             Dictionary whose values will be written into individual HDF5 datasets and whose corresponding keys provide
             the names of the datasets. You are recommended to limit these to simple and small datasets.
         kwargs: will be passed onto hdf_utils.write_main_dset() which will in turn will be passed onto the creation of
@@ -68,11 +68,19 @@ class NumpyTranslator(Translator):
             Absolute path of the written h5 file
 
         """
-        for arg in [h5_path, data_name, translator_name, quantity, units]:
-            assert isinstance(arg, (str, unicode))
-            assert len(arg) > 0
-        assert isinstance(raw_data, np.ndarray)
-        assert raw_data.ndim == 2
+        for arg, arg_name in zip([h5_path, data_name, translator_name, quantity, units],
+                                 ['h5_path', 'data_name', 'translator_name', 'quantity', 'units']):
+            if not isinstance(arg, (str, unicode)):
+                raise TypeError('{} should be of type - str; was of type - {}'.format(arg_name, type(arg)))
+            if not len(arg.strip()) == 0:
+                raise ValueError('{} should not be an empty string'.format(arg_name))
+
+        if not isinstance(raw_data, (np.ndarray, da.core.Array)):
+            raise TypeError('raw_data should either be a np.ndarray or a da.core.Array')
+
+        if raw_data.ndim != 2:
+            raise ValueError('raw_data should be a 2-dimensional matrix. Provided array was of shape: {}'
+                             '.'.format(raw_data.shape))
 
         for ind, dimensions in enumerate([pos_dims, spec_dims]):
             if isinstance(dimensions, Dimension):
@@ -83,7 +91,9 @@ class NumpyTranslator(Translator):
                 raise TypeError('dimensions should be a sequence of Dimension objects')
             # Check to make sure that the product of the position and spectroscopic dimension sizes match with
             # that of raw_data
-            assert raw_data.shape[ind] == np.product([len(x.values) for x in dimensions])
+            if raw_data.shape[ind] != np.product([len(x.values) for x in dimensions]):
+                raise ValueError('Size of dimension[{}] of raw_data not equal to product of size of dimensions: {}'
+                                 '.'.format(raw_data.shape[ind], np.product([len(x.values) for x in dimensions])))
 
         if extra_dsets is not None:
             if not isinstance(extra_dsets, dict):
@@ -131,3 +141,7 @@ class NumpyTranslator(Translator):
                 chan_grp.create_dataset(key.strip(), data=val)
 
         return h5_path
+
+
+class NumpyTranslator(ArrayTranslator):
+    pass
