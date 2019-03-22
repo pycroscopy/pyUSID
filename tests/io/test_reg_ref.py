1281a45cb62634c3bdaa7b5ee557a051fcb9f743
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-03-22 10:54:46 -0400

    Moved tests for clean_reg_ref, attempt_reg_ref_build to test_reg_ref

--- tests/io/test_reg_ref.py
+++ tests/io/test_reg_ref.py
@@ -12,7 +12,7 @@ import h5py
 import numpy as np
 from . import data_utils
 sys.path.append("../../pyUSID/")
-from pyUSID.io import hdf_utils, reg_ref
+from pyUSID.io import reg_ref
 
 
 test_h5_file_path = 'test_reg_ref.h5'
@@ -69,3 +69,110 @@ class TestRegRef(unittest.TestCase):
             h5_source = h5_f['/Raw_Measurement/source_main']
             returned = reg_ref.get_region(h5_source, 'even_rows')
             self.assertTrue(np.all(returned == h5_source[range(0, h5_source.shape[0], 2)]))
+            
+    def test_clean_reg_refs_1d(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Test', data=np.random.rand(7))
+            ref_in = (slice(0, None, 2))
+            cleaned = reg_ref.clean_reg_ref(h5_dset, ref_in)
+            self.assertEqual(ref_in, cleaned[0])
+        os.remove(file_path)
+
+    def test_clean_reg_refs_2d(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Test', data=np.random.rand(7, 5))
+            ref_in = (slice(0, None, 2), slice(None))
+            cleaned = reg_ref.clean_reg_ref(h5_dset, ref_in)
+            self.assertTrue(np.all([x == y for x, y in zip(ref_in, cleaned)]))
+        os.remove(file_path)
+
+    def test_clean_reg_refs_illegal_too_many_slices(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Test', data=np.random.rand(7, 5))
+            ref_in = (slice(0, None, 2), slice(None), slice(1, None, 2))
+            with self.assertRaises(ValueError):
+                _ = reg_ref.clean_reg_ref(h5_dset, ref_in)
+
+        os.remove(file_path)
+
+    def test_clean_reg_refs_illegal_too_few_slices(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Test', data=np.random.rand(7, 5))
+            ref_in = (slice(0, None, 2))
+            with self.assertRaises(ValueError):
+                _ = reg_ref.clean_reg_ref(h5_dset, ref_in)
+
+        os.remove(file_path)
+
+    def test_clean_reg_refs_out_of_bounds(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Test', data=np.random.rand(7, 5))
+            ref_in = (slice(0, 13, 2), slice(None))
+            expected = (slice(0, 7, 2), slice(None))
+            cleaned = reg_ref.clean_reg_ref(h5_dset, ref_in, verbose=False)
+            self.assertTrue(np.all([x == y for x, y in zip(expected, cleaned)]))
+        os.remove(file_path)
+
+    def test_attempt_reg_ref_build_spec(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Indices', data=np.random.rand(2, 5))
+            dim_names = ['Bias', 'Cycle']
+            expected = {'Bias': (slice(0, 1), slice(None)),
+                        'Cycle': (slice(1, 2), slice(None))}
+            if sys.version_info.major == 3:
+                with self.assertWarns(UserWarning):
+                    cleaned = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            else:
+                cleaned = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            for key, value in expected.items():
+                self.assertEqual(value, cleaned[key])
+        os.remove(file_path)
+
+    def test_attempt_reg_ref_build_pos(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Indices', data=np.random.rand(5, 2))
+            dim_names = ['Bias', 'Cycle']
+            expected = {'Bias': (slice(None), slice(0, 1)),
+                        'Cycle': (slice(None), slice(1, 2))}
+            if sys.version_info.major == 3:
+                with self.assertWarns(UserWarning):
+                    cleaned = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            else:
+                cleaned = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            for key, value in expected.items():
+                self.assertEqual(value, cleaned[key])
+        os.remove(file_path)
+
+    def test_attempt_reg_ref_build_pos_too_many_dims(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Indices', data=np.random.rand(5, 2))
+            dim_names = ['Bias', 'Cycle', 'Blah']
+            ret_val = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            self.assertEqual(ret_val, dict())
+        os.remove(file_path)
+
+    def test_attempt_reg_ref_build_pos_too_few_dims(self):
+        file_path = 'test.h5'
+        data_utils.delete_existing_file(file_path)
+        with h5py.File(file_path) as h5_f:
+            h5_dset = h5_f.create_dataset('Indices', data=np.random.rand(5, 2))
+            dim_names = ['Bias']
+            ret_val = reg_ref.attempt_reg_ref_build(h5_dset, dim_names)
+            self.assertEqual(ret_val, dict())
+        os.remove(file_path)
