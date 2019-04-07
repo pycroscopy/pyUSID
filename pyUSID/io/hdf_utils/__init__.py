3e897922ddcb9de276a0cf41d9b86afcdd9e8c72
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-04-07 02:47:51 -0400

    Broke hdf_utils into multiple files

---
+++ pyUSID/io/hdf_utils/__init__.py
@@ -0,0 +1,18 @@
+"""
+Utilities for reading and writing USID data in HDF5 files
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+    base
+    simple
+    model
+
+"""
+
+from .base import *
+from .simple import *
+from .model import *
