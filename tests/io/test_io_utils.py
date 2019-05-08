e4d44dd1d6714c30cfb382bbbd1c9855c7e1bd32
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-05-08 12:00:10 -0400

    Broke io_utils tests into classes

--- tests/io/test_io_utils.py
+++ tests/io/test_io_utils.py
@@ -13,12 +13,12 @@ from pyUSID.io import io_utils
 from pyUSID.processing import comp_utils
 
 
-class TestIOUtils(unittest.TestCase):
+class TestFormattedStrToNum(unittest.TestCase):
 
-    def test_formatted_str_to_number(self):
+    def test_typical(self):
         self.assertEqual(io_utils.formatted_str_to_number("4.32 MHz", ["MHz", "kHz"], [1E+6, 1E+3]), 4.32E+6)
 
-    def test_formatted_str_to_number_wrong_types(self):
+    def test_wrong_types(self):
         with self.assertRaises(TypeError):
             _ = io_utils.formatted_str_to_number({'dfdfd': 123}, ["MHz"], [1E+6])
         with self.assertRaises(TypeError):
@@ -28,7 +28,7 @@ class TestIOUtils(unittest.TestCase):
         with self.assertRaises(TypeError):
             _ = io_utils.formatted_str_to_number("4.32 MHz", ["MHz", "kHz"], [{'dfdfd': 13}, 1E+3])
 
-    def test_formatted_str_to_number_invalid(self):
+    def test_invalid(self):
         with self.assertRaises(ValueError):
             _ = io_utils.formatted_str_to_number("4.32 MHz", ["MHz"], [1E+6, 1E+3])
         with self.assertRaises(ValueError):
@@ -42,7 +42,10 @@ class TestIOUtils(unittest.TestCase):
         with self.assertRaises(ValueError):
             _ = io_utils.formatted_str_to_number("MHz", ["MHz", "kHz"], [1E+6, 1E+3])
 
-    def test_format_quantity(self):
+
+class TestFormatQuantity(unittest.TestCase):
+
+    def test_typical(self):
         qty_names = ['sec', 'mins', 'hours', 'days']
         qty_factors = [1, 60, 3600, 3600*24]
         ret_val = io_utils.format_quantity(315, qty_names, qty_factors)
@@ -50,7 +53,7 @@ class TestIOUtils(unittest.TestCase):
         ret_val = io_utils.format_quantity(6300, qty_names, qty_factors)
         self.assertEqual(ret_val, '1.75 hours')
 
-    def test_format_quantity_illegal(self):
+    def test_illegal(self):
         with self.assertRaises(ValueError):
             _ = io_utils.format_quantity(315, ['sec', 'mins', 'hours'], [1, 60, 3600, 3600*24])
         with self.assertRaises(ValueError):
@@ -59,6 +62,9 @@ class TestIOUtils(unittest.TestCase):
             _ = io_utils.format_quantity(315, ['sec', 14, 'hours'], [1, 60, 3600*24])
         with self.assertRaises(TypeError):
             _ = io_utils.format_quantity('hello', ['sec', 'mins', 'hours'], [1, 60, 3600])
+
+
+class TestTimeSizeFormatting(unittest.TestCase):
 
     def test_format_time(self):
         ret_val = io_utils.format_time(315)
@@ -71,6 +77,9 @@ class TestIOUtils(unittest.TestCase):
         self.assertEqual(ret_val, '15.23 bytes')
         ret_val = io_utils.format_size(5830418104.32)
         self.assertEqual(ret_val, '5.43 GB')
+
+
+class TestIOUtils(unittest.TestCase):
 
     def test_get_available_memory_rerouting(self):
         if sys.version_info.major == 3:
