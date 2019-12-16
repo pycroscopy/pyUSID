8a32d1e850a937bd331150b6df2eb6e5dacdd801
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-12-16 17:51:55 -0500

    Plugged a few testing holes for recommend_cores

--- tests/processing/test_comp_utils.py
+++ tests/processing/test_comp_utils.py
@@ -15,9 +15,9 @@ from pyUSID.processing import comp_utils
 MAX_CPU_CORES = cpu_count()
 
 
-class TestIOUtils(unittest.TestCase):
+class TestRecommendCPUCores(unittest.TestCase):
 
-    def test_recommend_cores_many_small_jobs(self):
+    def test_many_small_jobs(self):
         num_jobs = 14035
         if MAX_CPU_CORES > 4:
             min_free_cores = 2
@@ -32,26 +32,26 @@ class TestIOUtils(unittest.TestCase):
         ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=5000, lengthy_computation=False)
         self.assertEqual(ret_val, MAX_CPU_CORES)
 
-    def test_recommend_cores_changing_min_cores(self):
+    def test_changing_min_cores(self):
         num_jobs = 14035
         for min_free_cores in range(1, MAX_CPU_CORES):
             ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
             self.assertEqual(ret_val, max(1, MAX_CPU_CORES - min_free_cores))
 
-    def test_recommend_cores_illegal_min_free_cores(self):
+    def test_illegal_min_free_cores(self):
         num_jobs = 14035
         min_free_cores = MAX_CPU_CORES
         with self.assertRaises(ValueError):
             _ = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
 
-    def test_reccomend_cores_few_small_jobs(self):
+    def test_few_small_jobs(self):
         num_jobs = 13
         ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
         self.assertEqual(ret_val, 1)
         ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
         self.assertEqual(ret_val, 1)
 
-    def test_recommed_cores_few_large_jobs(self):
+    def test_few_large_jobs(self):
         num_jobs = 13
         if MAX_CPU_CORES > 4:
             min_free_cores = 2
@@ -62,7 +62,28 @@ class TestIOUtils(unittest.TestCase):
         ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES - 1, lengthy_computation=True)
         self.assertEqual(ret_val, max(1, MAX_CPU_CORES - 1))
 
-    def test_get_available_memory(self):
+    def test_invalid_min_cores(self):
+        with self.assertRaises(TypeError):
+            _ = comp_utils.recommend_cpu_cores(14035, min_free_cores=[4])
+
+    def test_invalid_requested_cores(self):
+        with self.assertRaises(TypeError):
+            _ = comp_utils.recommend_cpu_cores(14035, requested_cores=[4])
+
+    def test_invalid_num_jobs(self):
+        with self.assertRaises(TypeError):
+            _ = comp_utils.recommend_cpu_cores(14035.67)
+
+        with self.assertRaises(ValueError):
+            _ = comp_utils.recommend_cpu_cores(-14035)
+
+        with self.assertRaises(TypeError):
+            _ = comp_utils.recommend_cpu_cores('not a number')
+
+
+class TestGetAvailableMemory(unittest.TestCase):
+
+    def test_standard(self):
         from psutil import virtual_memory
         mem = virtual_memory().available
 
