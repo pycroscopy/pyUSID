59da44bba354181b0c8ce3bab1e549ce20f82f51
Raj <rgiri@uw.edu>
2020-02-19 17:38:54 -0800

    Proposed fix for the constant ValueError in USIDataset
    Minor comment string fix in process

--- pyUSID/io/usi_data.py
+++ pyUSID/io/usi_data.py
@@ -146,7 +146,7 @@ class USIDataset(h5py.Dataset):
             self.__n_dim_data_orig = self.get_n_dim_form(lazy=True)
             self.__n_dim_form_avail = True
             # TODO: This line keeps failing. Fix it
-            self.__n_dim_data_s2f = self.__n_dim_data_orig.transpose(self.__n_dim_sort_order_orig_s2f)
+            self.__n_dim_data_s2f = self.__n_dim_data_orig.transpose(np.ndarray(self.__n_dim_sort_order_orig_s2f))
         except ValueError:
             warn('This dataset does not have an N-dimensional form')
 
