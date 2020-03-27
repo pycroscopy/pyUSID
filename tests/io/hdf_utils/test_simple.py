ad90c624debbc5fdfaef57f980356c1b21eb1b14
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-03-26 22:05:03 -0400

    write_reduced_anc_dsets test write to new file

--- tests/io/hdf_utils/test_simple.py
+++ tests/io/hdf_utils/test_simple.py
@@ -310,6 +310,42 @@ class TestWriteReducedAncDsets(TestSimple):
 
         os.remove(duplicate_path)
 
+    def test_spec_2d_to_1d_new_file(self):
+        new_file_path = 'reduced_ancs.h5'
+        data_utils.delete_existing_file(new_file_path)
+        with h5py.File(data_utils.std_beps_path, mode='r') as h5_f:
+            h5_spec_inds_orig = h5_f['/Raw_Measurement/Spectroscopic_Indices']
+            h5_spec_vals_orig = h5_f['/Raw_Measurement/Spectroscopic_Values']
+            new_base_name = 'Blah'
+            with h5py.File(new_file_path, mode='w') as h5_f_new:
+
+                h5_spec_inds_new, h5_spec_vals_new = hdf_utils.write_reduced_anc_dsets(h5_f_new,
+                                                                                       h5_spec_inds_orig,
+                                                                                       h5_spec_vals_orig,
+                                                                                       'Bias',
+                                                                                       basename=new_base_name)
+
+                dim_names = ['Cycle']
+                dim_units = ['']
+                ref_data = np.expand_dims(np.arange(2), axis=0)
+                for h5_dset, exp_dtype, exp_name in zip([h5_spec_inds_new, h5_spec_vals_new],
+                                                        [h5_spec_inds_orig.dtype, h5_spec_vals_orig.dtype],
+                                                        [new_base_name + '_Indices', new_base_name + '_Values']):
+                    self.assertIsInstance(h5_dset, h5py.Dataset)
+                    self.assertEqual(h5_dset.parent, h5_f_new)
+                    self.assertEqual(h5_dset.name.split('/')[-1], exp_name)
+                    self.assertTrue(np.allclose(ref_data, h5_dset[()]))
+                    self.assertEqual(h5_dset.dtype, exp_dtype)
+                    self.assertTrue(np.all([_ in h5_dset.attrs.keys() for _ in ['labels', 'units']]))
+                    self.assertTrue(np.all([x == y for x, y in zip(dim_names, hdf_utils.get_attr(h5_dset, 'labels'))]))
+                    self.assertTrue(np.all([x == y for x, y in zip(dim_units, hdf_utils.get_attr(h5_dset, 'units'))]))
+                    # assert region references
+                    for dim_ind, curr_name in enumerate(dim_names):
+                        self.assertTrue(np.allclose(np.squeeze(ref_data[dim_ind]),
+                                                    np.squeeze(h5_dset[h5_dset.attrs[curr_name]])))
+
+        os.remove(new_file_path)
+
     def test_spec_1d_to_0d(self):
         duplicate_path = 'copy_test_hdf_utils.h5'
         data_utils.delete_existing_file(duplicate_path)
