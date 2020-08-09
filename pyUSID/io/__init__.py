62b83483eebca9b190a33f078bcaefb606b5d808
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-09 11:28:17 -0400

    Removed local copy of translator

--- pyUSID/io/__init__.py
+++ pyUSID/io/__init__.py
@@ -18,7 +18,6 @@ Submodules
 
 """
 from . import usi_data
-from . import translator
 from . import numpy_translator
 
 from . import hdf_utils
@@ -27,10 +26,10 @@ from . import dtype_utils
 from . import write_utils
 
 from .usi_data import USIDataset
-from .translator import Translator
 from .numpy_translator import NumpyTranslator, ArrayTranslator
 from .image import ImageTranslator
 from .write_utils import Dimension, DimType
+from sidpy.sid.translator import Translator
 
 __all__ = ['USIDataset', 'hdf_utils', 'io_utils', 'dtype_utils', 'NumpyTranslator', 'write_utils',
-           'ImageTranslator', 'Dimension', 'DimType', 'ArrayTranslator']
+           'ImageTranslator', 'Dimension', 'DimType', 'ArrayTranslator', 'Translator']
