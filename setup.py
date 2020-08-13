2c5716b78010b5bd0aeb474ad8c2c9cf340ffe98
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-13 15:45:49 -0400

    Adding support for python 3.8

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering :: Information Analysis'],
     keywords=['imaging', 'spectra', 'multidimensional', 'data format', 'universal', 'hdf5'],
