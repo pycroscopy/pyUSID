064dcd81d9c42f4eb4782f0a41fd437b3f56f50c
ssomnath <14300780+ssomnath@users.noreply.github.com>
2021-07-29 11:48:47 -0400

    Removed viz from pyUSID

--- pyUSID/__init__.py
+++ pyUSID/__init__.py
@@ -12,11 +12,8 @@ from . import io
 from .io import *
 from . import processing
 from .processing import *
-from sidpy import viz
-from sidpy.viz import plot_utils, jupyter_utils
 from .__version__ import version as __version__
 
 __all__ = ['__version__']
 __all__ += io.__all__
 __all__ += processing.__all__
-__all__ += viz.__all__
